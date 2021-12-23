ffbc13ba0e0546b7a012507a69662ea9dcc61823
kerim00000 <90604199+kerim00000@users.noreply.github.com>
2021-12-23 12:42:03 +0300

    Add files via upload

---
+++ medzhidov.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Profile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Анкета");  // Выводится надпись "Анкета"
+
+            Console.Write("Имя: ");
+            string name = Console.ReadLine(); // Программа считывает введенное имя 
+
+            Console.Write("Фамилия: ");
+            string surname = Console.ReadLine(); // Программа считывает введеную фамилию
+
+            Console.Write("Возраст: ");
+            string age = Console.ReadLine(); //  Программа считывает введенный возраст
+
+            Console.Write("Рост: ");
+            string height = Console.ReadLine(); // Программа считывает введенный рост
+
+            Console.Write("Вес: ");
+            string weight = Console.ReadLine(); // Программа считывает введенный вес
+
+            Console.WriteLine(name + " " + surname + " " + age + " " + height + " " + weight); // Выводятся данные анкеты используя склеивание
+            Console.WriteLine("{0} {1} {2} {3} {4}", name, surname, age, height, weight); // Выводятся данные анкеты используя формативный метод
+            Console.WriteLine($"{name} {surname} {age} {height} {weight}"); // Выводятся данные анкеты используя вывод со знаком $ 
+        }
+    }
+}
+
